904a6b90384b29f2527a1f959173d786ce8d2c35
Tais1990 <tais_kat@list.ru>
2019-04-26 22:47:11 +0500

    add test

---
+++ XUnitTestNetwork/Model/VectorTest.cs
@@ -0,0 +1,14 @@
+﻿using Network.Models;
+using Xunit;
+
+namespace XUnitTestNetwork.Model
+{
+    public class VectorTest
+    {
+        [Fact]
+        public void CanInstantiateVectorEmpty()
+        {
+            Vector vector = new Vector();
+        }
+    }
+}
