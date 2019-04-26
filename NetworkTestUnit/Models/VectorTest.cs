ce7b6b79430230a37cef4f48250baf7eb8aeacd4
Tais1990 <tais_kat@list.ru>
2019-04-26 15:35:06 +0500

    change tast file

--- NetworkTestUnit/Models/VectorTest.cs
+++ NetworkTestUnit/Models/VectorTest.cs
@@ -1,26 +1,24 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
-using Network.Models;
-
-namespace NetworkTestUnit
+namespace Network.Models.Tests
 {
-    [TestClass]
+    [TestClass()]
     public class VectorTest
     {
-        [TestMethod]
+        [TestMethod()]
         public void CanInstantiateVectorEmpty()
         {
             Vector vector = new Vector();
         }
-        [TestMethod]
+        [TestMethod()]
         public void CanInstantiateVectorWithCoordinates()
         {
             Vector vector = new Vector(-5, 9);
             Assert.AreEqual(vector.x, -5);
             Assert.AreEqual(vector.y, 9);
         }
-        [TestMethod]
+        [TestMethod()]
         public void CanInstantiateVectorCopy()
         {
             Vector vector = new Vector(1, 9);
@@ -28,13 +26,13 @@ namespace NetworkTestUnit
             Assert.AreEqual(vectorNew.x, 1);
             Assert.AreEqual(vectorNew.y, 9);
         }
-        [TestMethod]
+
         public void CalcueteMod()
         {
             Vector vector = new Vector(1, 1);
             Assert.AreEqual(vector.mod(), Math.Sqrt(2));
         }
-        [TestMethod]
+        [TestMethod()]
         public void CalcueteModInt()
         {
             Vector vector = new Vector(4, -3);
@@ -42,70 +40,70 @@ namespace NetworkTestUnit
         }
         //[TestInitialize]
         // тут может быть прописано то, что должно выполниться перед всеми тестами. только обязательно public
-        [TestMethod]
+        [TestMethod()]
         public void Multiplication1()
         {
             Vector a = new Vector(1, 2);
             Vector b = new Vector(-9, 5);
             Assert.AreEqual(a * b, 1);
         }
-        [TestMethod]
+        [TestMethod()]
         public void Multiplication2()
         {
             Vector a = new Vector(1, 2);
             Vector b = new Vector(9, 5);
             Assert.AreEqual(a * b, 19);
         }
-        [TestMethod]
+        [TestMethod()]
         public void AngleBeteen1()
         {
             Vector a = new Vector(10, 0);
             Vector b = new Vector(0, 10);
             Assert.AreEqual(a ^ b, Math.PI * 0.5);
         }
-        [TestMethod]
+        [TestMethod()]
         public void AngleBeteen2()
         {
             Vector a = new Vector(10, 0);
             Vector b = new Vector(1, 0);
             Assert.AreEqual(a ^ b, 0);
         }
-        [TestMethod]
+        [TestMethod()]
         public void AngleBeteen3()
         {
             Vector a = new Vector(10, 0);
             Vector b = new Vector(-1, 0);
             Assert.AreEqual(a ^ b, Math.PI);
         }
-        [TestMethod]
+        [TestMethod()]
         public void AngleBeteen4()
         {
             Vector a = new Vector(1, 2);
             Vector b = new Vector(-2, 1);
             Assert.AreEqual(a ^ b, Math.PI * 0.5);
         }
-        [TestMethod]
+        [TestMethod()]
         public void AngleBeteen5()
         {
             Vector a = new Vector(1, 2);
             Vector b = new Vector(-2, 1);
             Assert.AreEqual(b ^ a, Math.PI * (-0.5));
         }
-        [TestMethod]
+        [TestMethod()]
         public void AngleBeteen6()
         {
             Vector a = new Vector(1, 2);
             Vector b = new Vector(2, -1);
             Assert.AreEqual(a ^ b, Math.PI * (-0.5));
         }
-        [TestMethod]
+        [TestMethod()]
         public void AngleBeteen7()
         {
             Vector a = new Vector(1, 2);
             Vector b = new Vector(-1, -2);
             Assert.AreEqual(a ^ b, Math.PI, MyMath.Epsilon);
         }
-        [TestMethod]
+        [TestMethod()]
         public void Plus1()
         {
             Vector a = new Vector(1, 2);
@@ -114,7 +112,7 @@ namespace NetworkTestUnit
             Assert.AreEqual(c.x, 2);
             Assert.AreEqual(c.y, 4);
         }
-        [TestMethod]
+        [TestMethod()]
         public void Plus2()
         {
             Vector a = new Vector(1, 2);
@@ -123,7 +121,7 @@ namespace NetworkTestUnit
             Assert.AreEqual(c.x, 2);
             Assert.AreEqual(c.y, 0);
         }
-        [TestMethod]
+        [TestMethod()]
         public void Minus1()
         {
             Vector a = new Vector(1, 2);
@@ -132,7 +130,7 @@ namespace NetworkTestUnit
             Assert.AreEqual(c.x, 0);
             Assert.AreEqual(c.y, 0);
         }
-        [TestMethod]
+        [TestMethod()]
         public void Minus2()
         {
             Vector a = new Vector(1, 2);
@@ -141,7 +139,7 @@ namespace NetworkTestUnit
             Assert.AreEqual(c.x, 0);
             Assert.AreEqual(c.y, 4);
         }
-        [TestMethod]
+        [TestMethod()]
         public void Rotation1()
         {
             Vector vector = new Vector(1, 2);
@@ -149,7 +147,7 @@ namespace NetworkTestUnit
             Assert.AreEqual(v.x, -2, MyMath.Epsilon);
             Assert.AreEqual(v.y, 1, MyMath.Epsilon);
         }
-        [TestMethod]
+        [TestMethod()]
         public void Rotation2()
         {
             Vector vector = new Vector(1, 2);
@@ -157,7 +155,7 @@ namespace NetworkTestUnit
             Assert.AreEqual(v.x, 2, MyMath.Epsilon);
             Assert.AreEqual(v.y, -1, MyMath.Epsilon);
         }
-        [TestMethod]
+        [TestMethod()]
         public void Rotation3()
         {
             Vector vector = new Vector(1, 2);
@@ -165,7 +163,7 @@ namespace NetworkTestUnit
             Assert.AreEqual(v.x, -1, MyMath.Epsilon);
             Assert.AreEqual(v.y, -2, MyMath.Epsilon);
         }
-        [TestMethod]
+        [TestMethod()]
         public void Rotation4()
         {
             Vector vector = new Vector(1, Math.Sqrt(3));
@@ -173,7 +171,7 @@ namespace NetworkTestUnit
             Assert.AreEqual(v.x, 0, MyMath.Epsilon);
             Assert.AreEqual(v.y, 2, MyMath.Epsilon);
         }
-        [TestMethod]
+        [TestMethod()]
         public void Rotation5()
         {
             Vector vector = new Vector(1, Math.Sqrt(3));
